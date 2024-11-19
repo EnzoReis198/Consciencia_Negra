32c88394a4b91855078853bc7e3f955f3c086d1f
unknown <fearaujo871@gmail.com>
2024-11-18 23:58:13 -0300

    implementacao + correcao bugs e erros selects saibaMaisSobrePaises.js

--- assets/js/vejaMaisSobrePaises.js
+++ assets/js/vejaMaisSobrePaises.js
@@ -25,56 +25,60 @@ window.onload = function() {
 paisSelect.addEventListener('change', function() {
     const paisId = parseInt(this.value);
 
-    console.log('paisID:',paisId)
+    // console.log('paisID:',paisId) 
 
-    // Limpar os selects de artistas e comidas ao selecionar um novo país
-    
-
-
-    // artistaSelect.innerHTML = '<option value="">Selecione um artista</option>';
-    // comidaSelect.innerHTML = '<option value="">Selecione uma comida típica</option>';
-    // artistaSelect.disabled = !paisId;
-    // comidaSelect.disabled = !paisId;
+    // Limpa as opções anteriores antes de adicionar as novas
+    artistaSelect.innerHTML = '<option value="">Selecione um artista</option>';
+    comidaSelect.innerHTML = '<option value="">Selecione uma comida típica</option>';
+    artistaSelect.disabled = !paisId;
+    comidaSelect.disabled = !paisId;
 
     if(paisId){
-        // const artistas = bancoDados.artistas.filter(artista => artista.paisId === paisId);//ainda n tem
-        // const comidas = bancoDados.comidas.filter(comida => comida.paisId === paisId); //ainda n tem
+        const artistas = bancoDados.artistas.filter(artista => artista.id_pais === paisId);
+        const comidas = bancoDados.comidas.filter(comida => comida.id_pais === paisId); 
+        // console.log('artistas:',artistas)
 
-        // artistas.forEach(artista => {
-        //     let option = document.createElement('option');
-        //     option.value = artista.id;
-        //     option.textContent = artista.nome;
-        //     artistaSelect.appendChild(option);
-        // });
+        artistas.forEach(artista => {
+            let option = document.createElement('option');
+            option.value = artista.id_pais;
+            option.textContent = artista.nome_artista;
+            artistaSelect.appendChild(option);
+        });
 
-        // comidas.forEach(comida => {
-        //     let option = document.createElement('option');
-        //     option.value = comida.id;
-        //     option.textContent = comida.nome;
-        //     comidaSelect.appendChild(option);
-        // });
-        console.log('passei por if(paisesId)');
+        comidas.forEach(comida => {
+            let option = document.createElement('option');
+            option.value = comida.id_pais;
+            option.textContent = comida.nome_comida;
+            comidaSelect.appendChild(option);
+        });
+        // console.log('passei por if(paisesId)');
     };
  });
 
  btnBuscar.addEventListener('click', function() {
     const paisId = parseInt(paisSelect.value);
-    // const artistaId = parseInt(artistaSelect.value);//ainda n tem
-    // const comidaId = parseInt(comidaSelect.value);//ainda n tem
-    resultadoDiv.innerHTML = ''; // Limpar resultado anterior
+    const artistaId = parseInt(artistaSelect.value);
+    const comidaId = parseInt(comidaSelect.value);
+    resultadoDiv.innerHTML = ''; 
+
+    if(!paisId){
+        resultadoDiv.style.display = 'none'; 
+        return; 
+    }
 
     if (paisId) {
         const pais = bancoDados.paises.find(p => p.id === paisId);
         resultadoDiv.innerHTML += `<p class='resultado-p'><strong>País:</strong> ${pais.descricao}<p>`;
     }
-    // if (artistaId) {
-    //     const artista = bancoDados.artistas.find(a => a.id === artistaId);
-    //     resultadoDiv.innerHTML += `<div><strong>Artista:</strong> ${artista.descricao}</div>`;
-    // }
-    // if (comidaId) {
-    //     const comida = bancoDados.comidas.find(c => c.id === comidaId);
-    //     resultadoDiv.innerHTML += `<div><strong>Comida:</strong> ${comida.descricao}</div>`;
-    // }
+    if (artistaId) {
+        const artista = bancoDados.artistas.find(a => a.id_pais === artistaId);
+        resultadoDiv.innerHTML += `<p class='resultado-p'><strong>Artista:</strong> ${artista.descricao}</p>`;
+    }
+    if (comidaId) {
+        const comida = bancoDados.comidas.find(c => c.id_pais === comidaId);
+        resultadoDiv.innerHTML += `<p class='resultado-p'><strong>Comida:</strong> ${comida.descricao}</p>`;
+    }
     resultadoDiv.style.display = 'block';
+
 
 });
